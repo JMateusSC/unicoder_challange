9193926aff43b13bbad32eaa5926776c774878a7
JMateusSC <mateuscastro308@gmail.com>
2024-06-12 20:50:27 -0300

    Update url

--- Frontend/resources/js/app.js
+++ Frontend/resources/js/app.js
@@ -22,7 +22,7 @@ function create_task(params) {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            window.location.replace("/");
+            window.location.replace("/home");
         }
         else
         {
